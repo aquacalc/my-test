7ef94c90818d7e6c33c07e0545b0c5ea8af47d73
aquacalc <aquacalc@yahoo.com>
2025-01-03 15:24:26 -0500

    analytics injection for SK

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
-  import "../app.css";
-  
-  import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+	import '../app.css';
 </script>
 
 <slot />
