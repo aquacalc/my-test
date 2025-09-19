8706f9324eae24eaaa5a996047f481180bd2586f
aquacalc <aquacalc@yahoo.com>
2025-09-19 16:04:05 -0400

    ∆ font wt

--- src/routes/endorsements/+page.svelte
+++ src/routes/endorsements/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="container mx-auto">
 	<p class="m-28 mb-5 mx-10 md:mx-24 lg:mx-48 text-3xl md:text-5xl text-blue-500 text-center">
-		Real Stories &mdash; Real <em>Results</em>
+		Real Stories &mdash; Real <em class="font-medium">Results</em>
 	</p>
 	<p class="pt-2 mb-8 mx-10 md:mx-24 lg:mx-8 text-2xl text-gray-700 text-center">
 		See why my customers trust me with one of the biggest decisions of their lives.
